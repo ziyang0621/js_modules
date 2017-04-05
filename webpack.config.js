270082daf75e6a1c3fbd717cfc6c8657ef7879f2
Ziyang tan <ziyang0621@gmail.com>
2017-04-04 20:50:25 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "build/"
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/
+      },
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: "css-loader"
+        }),
+        test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: { limit: 40000 }
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new ExtractTextPlugin("style.css")]
+};
+
+module.exports = config;
